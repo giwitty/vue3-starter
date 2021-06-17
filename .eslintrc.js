a5154aa34eeb3be52a102f17f82cf10bf521ed5f
陈强 <chenqiang@wondersoft.cn>
2021-06-17 15:18:51 +0800

    feat: 集成commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,26 @@ module.exports = {
   },
   plugins: ['vue', '@typescript-eslint'],
   rules: {
-    'vue/no-multiple-template-root': 0
+    'vue/no-multiple-template-root': 0,
+    'import/extensions': 0,
+    // 'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+    'import/no-unresolved': 0,
+    'no-plusplus': 0,
+    // 尽量不要直接修改入参，如果入参是对象，可能会导致对象的属性被覆盖
+    'no-param-reassign': [
+      'warn',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'e', // for e.returnvalue
+          'ctx', // for Koa routing
+          'req', // for Express requests
+          'request', // for Express requests
+          'res', // for Express responses
+          'response', // for Express responses
+          'state' // for vuex state
+        ]
+      }
+    ]
   }
 }
